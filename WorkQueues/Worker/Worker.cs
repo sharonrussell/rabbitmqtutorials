db8148b8dab4039d31bf063efd6468eb973ed31e
sharonrussell <sharon.russell@comparethemarket.com>
2018-01-12 16:12:07 +0000

    initial commit

---
+++ WorkQueues/Worker/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+// ReSharper disable AccessToDisposedClosure
+
+namespace Worker
+{
+    public class Worker
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    var dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+
+                channel.BasicConsume(queue: "task_queue",
+                    autoAck: false,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
